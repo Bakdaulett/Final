2bea6881654db79c05005deb3d483b16d2a20be9
Sabyrzhan <sayatbek@snoonu.com>
2022-12-16 15:24:33 +0600

    style is changed by Sabr

--- reg.js
+++ reg.js
@@ -1,3 +1,113 @@
+//index js
+
+$(document).ready(function(){
+
+    $("#tagline-container > div").removeClass("opaque");
+
+    $(document).mousemove(function(event){
+        $(".cursor1").css({
+            "top": event.pageY,
+            "left": event.pageX
+        });
+    });
+
+
+
+    $(".mouse-bulge").mouseenter(function(){
+        $(".cursor1, .cursor2").animate({
+            opacity: "0.5",
+            height: "45px",
+            width: "45px"
+        }, 50);
+    }).mouseleave(function(){
+        $(".cursor1, .cursor2").animate({
+            opacity: "0.7",
+            height: "25px",
+            width: "25px"
+        }, 50);
+    });
+
+
+    //console.log($("#overlay-navbar").height());
+
+
+
+    $("#overlay-trigger").click(function(){
+        $(this).toggleClass("open");
+
+        //$("#overlay-navbar").css("height", "100%");
+
+        if($("#overlay-navbar").height() == 0){
+            $("#overlay-navbar").animate({
+                height: '100%'
+            }, 300);
+
+            $(".cursor1").addClass("hidden");
+            $(".cursor2").removeClass("hidden");
+
+            $(document).mousemove(function(event2){
+                $(".cursor2").css({
+                    "top": event2.pageY,
+                    "left": event2.pageX
+                });
+            });
+
+        } else {
+            $("#overlay-navbar").animate({
+                height:'0'
+            }, 300);
+
+            $(".cursor2").addClass("hidden");
+            $(".cursor1").removeClass("hidden");
+
+        }
+    });
+
+    $(window).scroll(function(){
+        $(".show-me").each(function(raju){
+            var base_of_object = $(this).offset().top + $(this).outerHeight();
+            var base_of_window = $(window).scrollTop() + $(window).height();
+
+            if(base_of_window > base_of_object){
+                $(this).animate({'opacity':'1'},1000);
+            }
+        })
+    });
+
+    $("#left-angle").click(function(){
+
+        $("#services-list").animate({
+            scrollLeft: $("#services-list").scrollLeft()-810
+        }, 300);
+
+    });
+
+    $("#right-angle").click(function(){
+        $("#services-list").animate({
+            scrollLeft: $("#services-list").scrollLeft()+810
+        }, 300);
+    });
+
+    $("#down").click(function(e){
+
+        if(this.hash !== ""){
+            e.preventDefault();
+        }
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 300, function(){
+            window.location.hash = hash;
+        });
+
+    });
+
+});
+
+
+// registration
 const container = document.querySelector(".container"),
     passwordHideShow = document.querySelectorAll(".showHidePw"),
     passwordFields = document.querySelectorAll(".password"),
